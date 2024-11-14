bac448a0f3f5fe99cdf7885d9ea15885bc2eaad9
HemangVora <hemangvora2@gmail.com>
2024-11-14 15:06:09 +0700

    resolved build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/ui/Navbar";
-import Image from "next/image";
 import {
   Card,
   CardContent,
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { SVGProps } from "react";
-
 export default function Navbar() {
   return (
     <nav className={"fixed inset-x-0 top-0 z-50  shadow-sm  text-white "}>
@@ -99,24 +95,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  );
-}
-
-function MountainIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   );
 }
